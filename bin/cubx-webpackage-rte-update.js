6d3c127712b2cb1a9361f34f7038da219ac68d4a
edwingamboa <edwingamboa355@gmail.com>
2018-04-07 14:25:12 -0500

    cubbles/cubbles-coder-devtools#6: Add bin script

---
+++ bin/cubx-webpackage-rte-update.js
@@ -0,0 +1,23 @@
+'use strict';
+var RteUpdater = require('../lib/cubx-webpackage-rte-update');
+var commandLineArgs = require('command-line-args');
+
+var optionDefinitions = [
+  { name: 'webpackagePath', type: String, alias: 'p' },
+  { name: 'newRteVersion', type: String, alias: 'v' }
+];
+
+var options = commandLineArgs(optionDefinitions);
+
+if (!options.webpackagePath) {
+  console.error('Missed necessary parameter "webpackagePath". Usage: cubx-webpackage-rte-update -p <webpackagPath>');
+  process.exit(0);
+}
+
+if (!options.newRteVersion) {
+  console.error('Missed necessary parameter "newRteVersion". Usage: cubx-webpackage-rte-update -v <newRteVersion>');
+  process.exit(0);
+}
+
+var rteUpdater = new RteUpdater(options.webpackagePath);
+rteUpdater.updateRteInWebpackage(options.newRteVersion);
