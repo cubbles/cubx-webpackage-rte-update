33a9c8cdbe3a33146d4a74fcf84d4703de863506
edwingamboa <edwingamboa355@gmail.com>
2018-04-07 14:00:08 -0500

    cubbles/cubbles-coder-devtools#6: Add test for new _updateRteInArtifacts method

--- test/unit/RteUpdater_test.js
+++ test/unit/RteUpdater_test.js
@@ -25,12 +25,12 @@
       fs.emptyDirSync(wpPath);
       fs.copySync(wpBackupPath, wpPath);
     });
-    describe('#_updateRteInArtifactsRunnables', function () {
+    describe('#_updateRteInArtifacts', function () {
       beforeEach(function () {
         rteUpdater.manifest = JSON.parse(fs.readFileSync(wpManifestPath, 'utf8'));
       });
       it('should update rte version in all runnables of all artifacts', function () {
-        rteUpdater._updateRteInArtifactsRunnables(newArtifactVersion);
+        rteUpdater._updateRteInArtifacts(newArtifactVersion);
         var refactoredApp = fs.readFileSync(path.join(wpPath, 'app', 'index.html'), 'utf8');
         var expectedApp = fs.readFileSync(path.join(wpPath, 'app', refactoredFilesFolderName, 'index.html'), 'utf8');
         var refactoredDocsApp = fs.readFileSync(path.join(wpPath, 'docs', 'index.html'), 'utf8');
@@ -41,10 +41,18 @@
         var refactoredCompoundDocs = fs.readFileSync(path.join(wpPath, 'my-compound', 'docs', 'index.html'), 'utf8');
         var expectedCompoundDocs = fs.readFileSync(path.join(wpPath, 'my-compound', 'docs', refactoredFilesFolderName, 'index.html'), 'utf8');
 
-        var refactoredElementaryDemo = fs.readFileSync(path.join(wpPath, 'my-compound', 'demo', 'index.html'), 'utf8');
-        var expectedElementaryDemo = fs.readFileSync(path.join(wpPath, 'my-compound', 'demo', refactoredFilesFolderName, 'index.html'), 'utf8');
+        var refactoredElementaryDemo = fs.readFileSync(path.join(wpPath, 'my-elementary', 'demo', 'index.html'), 'utf8');
+        var expectedElementaryDemo = fs.readFileSync(path.join(wpPath, 'my-elementary', 'demo', refactoredFilesFolderName, 'index.html'), 'utf8');
         var refactoredElementaryDocs = fs.readFileSync(path.join(wpPath, 'my-elementary', 'docs', 'index.html'), 'utf8');
         var expectedElementaryDocs = fs.readFileSync(path.join(wpPath, 'my-elementary', 'docs', refactoredFilesFolderName, 'index.html'), 'utf8');
+
+        var refactoredElementary1Demo = fs.readFileSync(path.join(wpPath, 'my-elementary-1', 'demo', 'index.html'), 'utf8');
+        var expectedElementary1Demo = fs.readFileSync(path.join(wpPath, 'my-elementary-1', 'demo', refactoredFilesFolderName, 'index.html'), 'utf8');
+        var refactoredElementary1Docs = fs.readFileSync(path.join(wpPath, 'my-elementary-1', 'docs', 'index.html'), 'utf8');
+        var expectedElementary1Docs = fs.readFileSync(path.join(wpPath, 'my-elementary-1', 'docs', refactoredFilesFolderName, 'index.html'), 'utf8');
+
+        var refactoredManifest = fs.readFileSync(wpManifestPath, 'utf8');
+        var expectedManifest = fs.readFileSync(path.join(wpPath, refactoredFilesFolderName, 'manifest.webpackage'), 'utf8');
 
         return Promise.all([
           expect(refactoredApp).to.be.equal(expectedApp),
@@ -52,7 +60,10 @@
           expect(refactoredCompoundDemo).to.be.equal(expectedCompoundDemo),
           expect(refactoredCompoundDocs).to.be.equal(expectedCompoundDocs),
           expect(refactoredElementaryDemo).to.be.equal(expectedElementaryDemo),
-          expect(refactoredElementaryDocs).to.be.equal(expectedElementaryDocs)
+          expect(refactoredElementaryDocs).to.be.equal(expectedElementaryDocs),
+          expect(refactoredElementary1Demo).to.be.equal(expectedElementary1Demo),
+          expect(refactoredElementary1Docs).to.be.equal(expectedElementary1Docs),
+          expect(refactoredManifest).to.be.equal(expectedManifest)
         ]);
       });
     });
